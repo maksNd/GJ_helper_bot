3a1c746efd4c85afc8deb02a30a3cbfd3a8dd82a
mkN <mknord1565@gmail.com>
2022-07-17 12:55:56 +0300

    improvements

--- bot.py
+++ bot.py
@@ -1,42 +1,28 @@
 import telebot
-from telebot import types
 from reading_json import reading_json_file
+from constants import TOKEN
+from utils import create_keybord_for_zones
 
-# filepath = r'data_for_all_zones1.json'
-filepath = r'positions_for_all_zones.json'
+filepath = r'data_for_all_zones1.json'
 
 dict_from_json = reading_json_file(filepath)
 
-token = '5471219208:AAFIjOrKvPFINV3NykFhKC74rrGT0Ea0-ic'
-bot = telebot.TeleBot(token, parse_mode=None)
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
-
-keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-button_f01 = types.KeyboardButton('F01')
-button_f02 = types.KeyboardButton('F02')
-button_f03 = types.KeyboardButton('F03')
-button_f04 = types.KeyboardButton('F04')
-button_f05 = types.KeyboardButton('F05')
-button_f06 = types.KeyboardButton('F06')
-button_f07 = types.KeyboardButton('F07')
-button_f08 = types.KeyboardButton('F08')
-button_start = types.KeyboardButton('/start')
-
-keyboard.add(button_f01, button_f02, button_f03, button_f04, button_f05, button_f06, button_f07, button_f08,
-             button_start)
+# keyboard_for_zones = create_keybord_for_zones()
 
 
 @bot.message_handler(commands=['start'])
 def reply_for_start_command(message):
-    bot.send_message(message.chat.id, 'Введите позицию для поиска \n(4х значное число)')
+    bot.send_message(message.chat.id, 'Введите позицию для поиска IGMI\n(4х значное число)')
 
 
 @bot.message_handler()
 def show_buttons(message):
-    if len(message.text) == 4 and not message.text.isalpha():
+    if len(message.text) == 4 and message.text.isdigit():
         global position
         position = message.text
-        bot.send_message(message.chat.id, "Выберите зону", reply_markup=keyboard)
+        bot.send_message(message.chat.id, "Выберите зону", reply_markup=create_keybord_for_zones())
 
     if message.text in ('F01', 'F02', 'F03', 'F04', 'F05', 'F06', 'F07', 'F08'):
         zone = message.text
---
+++ utils.py
@@ -0,0 +1,9 @@
+from telebot import types
+
+def create_keybord_for_zones():
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    dict_with_buttons = {}
+    for i in range(1, 9):
+        dict_with_buttons[i] = types.KeyboardButton(f'F0{i}')
+    keyboard.add(*dict_with_buttons.values(), types.KeyboardButton('/start'))
+    return keyboard
