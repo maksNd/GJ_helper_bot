b24f55bb6321b1d634dee629dde2a912857405f9
mkN <mknord1565@gmail.com>
2022-07-17 23:30:44 +0300

    avoid global

--- bot.py
+++ bot.py
@@ -9,21 +9,25 @@ bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
 @bot.message_handler(commands=['start'])
 def reply_for_start_command(message):
-    bot.send_message(message.chat.id, 'Введите позицию для поиска IGMI\n(4х значное число)')
+    bot.send_message(message.chat.id, 'Введите позицию для поиска EGMI\n(4х значное число)')
+
+
+position_to_find = {'position': '0000'}  # инициализируем переменную для хранения позиции для поиска
 
 
 @bot.message_handler()
 def show_buttons(message):
     if len(message.text) == 4 and message.text.isdigit():
-        global position
-        position = message.text
+        position_to_find['position'] = message.text  # если ввод похож на позицию для поиска - переопределим переменную
         bot.send_message(message.chat.id, "Выберите зону", reply_markup=create_keybord_for_zones())
+
+    position = position_to_find['position']
 
     if message.text in ('F01', 'F02', 'F03', 'F04', 'F05', 'F06', 'F07', 'F08'):
         zone = message.text
         try:
             dict_from_json = load_from_gitHub_json_file()
-            bot.send_message(message.chat.id, f"{zone}.{position} - {dict_from_json[zone][f'{zone}.{position}'][-4:]}")
+            bot.send_message(message.chat.id, f"{zone}.{position} - {dict_from_json[zone][f'{zone}.{position}']}")
         except KeyError:
             bot.send_message(message.chat.id, f'Не могу найти позицию - {zone}.{position}')
         except NameError:
