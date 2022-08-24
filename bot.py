dbb5e8cce467b4b76d33e5859e50a7d7406d7e3f
mk_work <mknord1565@gmail.com>
2022-08-24 13:16:25 +0300

    extension menu; add buttons "Yesterday", "Today", "Tomorrow"

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ from telebot import telebot, types
 from constants import TOKEN
 from utils_for_igme_positions import load_from_gitHub_json_file, looks_like_conveyor_position
 from utils_for_show_change import conver_str_to_date, calculate_change_for_date, looks_like_date
-from utils_for_bot import create_keybord_for_zones
+from utils_for_bot import create_keybord_for_zones, create_keyboard_for_change
 import logging
-from logger import get_and_set_logger
+from logger import get_and_set_logger, new_log
 
 get_and_set_logger()
 logger = logging.getLogger('basic')
@@ -28,11 +28,11 @@ def reply_for_log_command(message):
     bot.send_message(message.chat.id, data_from_log)
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'cancel'])
 def reply_for_start_command(message):
     start_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button_1 = types.KeyboardButton('Find IGME position')
-    button_2 = types.KeyboardButton('Change today')
+    button_2 = types.KeyboardButton('Change')
     start_keyboard.add(button_1, button_2)
     bot.send_message(message.chat.id, 'ok', reply_markup=start_keyboard)
 
@@ -54,29 +54,51 @@ def main_message_handler(message):  # перехватчик всех сообщ
         except ValueError:
             return
         change_for_date = calculate_change_for_date(date_for_calculate)
-        logger.info(f'{message.from_user.id} - get reply for change by date')
+        new_log(logger, message.from_user.first_name, message.text)
         bot.send_message(message.chat.id, f'{date_for_calculate.strftime("%d.%m.%y")}:\n{change_for_date}')
 
-    if message.text == 'Change today':  # нажата кнопка "Change today"
+    if message.text == 'Change':  # нажата кнопка "Change"
+        keyboard = create_keyboard_for_change()
+        bot.send_message(message.chat.id, f'Choice the day',
+                         reply_markup=keyboard)
+
+    if message.text == 'Today':  # нажата кнопка "Today"
         date_for_calculate = datetime.datetime.now().date()
         change_for_date = calculate_change_for_date(date_for_calculate)
-        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        start_button = types.KeyboardButton('/start')
-        keyboard.add(start_button)
-        logger.info(f'{message.from_user.id} - get reply for change today')
+        keyboard = create_keyboard_for_change()
+        new_log(logger, message.from_user.first_name, message.text)
+        bot.send_message(message.chat.id, f'{date_for_calculate.strftime("%d.%m.%y")}:\n{change_for_date}',
+                         reply_markup=keyboard)
+
+    if message.text == 'Yesterday':  # нажата кнопка "Yesterday"
+        date_for_calculate = datetime.datetime.now().date() - datetime.timedelta(days=1)
+        change_for_date = calculate_change_for_date(date_for_calculate)
+        keyboard = create_keyboard_for_change()
+        new_log(logger, message.from_user.first_name, message.text)
+        bot.send_message(message.chat.id, f'{date_for_calculate.strftime("%d.%m.%y")}:\n{change_for_date}',
+                         reply_markup=keyboard)
+
+    if message.text == 'Tomorrow':  # нажата кнопка "Yesterday"
+        date_for_calculate = datetime.datetime.now().date() + datetime.timedelta(days=1)
+        change_for_date = calculate_change_for_date(date_for_calculate)
+        keyboard = create_keyboard_for_change()
+        new_log(logger, message.from_user.first_name, message.text)
         bot.send_message(message.chat.id, f'{date_for_calculate.strftime("%d.%m.%y")}:\n{change_for_date}',
                          reply_markup=keyboard)
 
     if looks_like_conveyor_position(message.text):  # введен текст похожий на позицию конвейера
-        position_to_find['position'] = message.text  # если ввод похож на позицию для поиска - переопределим переменную
+        position_to_find['position'] = message.text  # переопределим переменную
         keyboard = create_keybord_for_zones()
-        bot.send_message(message.chat.id, "Change zone", reply_markup=keyboard)
+        bot.send_message(message.chat.id, "Choice zone", reply_markup=keyboard)
 
     position = position_to_find['position']
 
     if message.text == 'Find IGME position':  # нажата кнопка "Find IGME position"
-        remote_keyboard = types.ReplyKeyboardRemove()  # удалит клавиатуру
-        bot.send_message(message.chat.id, "Write conveyor position\n(4 signs)", reply_markup=remote_keyboard)
+        # remote_keyboard = types.ReplyKeyboardRemove()  # удалит клавиатуру
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        cancel_button = types.KeyboardButton('/cancel')
+        keyboard.add(cancel_button)
+        bot.send_message(message.chat.id, "Write conveyor position\n(4 signs)", reply_markup=keyboard)
 
     if message.text in ('F01', 'F02', 'F03', 'F04', 'F05', 'F06', 'F07', 'F08'):  # нажата кнопка зоны"
         zone = message.text
--- utils_for_bot.py
+++ utils_for_bot.py
@@ -2,10 +2,21 @@ from telebot import types
 
 
 def create_keybord_for_zones():
-    """Create buttons for all zones with /start command"""
+    """Create buttons for all zones with /cancel command"""
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     dict_with_buttons = {}
     for i in range(1, 9):
         dict_with_buttons[i] = types.KeyboardButton(f'F0{i}')
-    keyboard.add(*dict_with_buttons.values(), types.KeyboardButton('/start'))
+    keyboard.add(*dict_with_buttons.values(), types.KeyboardButton('/cancel'))
+    return keyboard
+
+
+def create_keyboard_for_change():
+    """Create buttons for choice change"""
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    cancel_button = types.KeyboardButton('/cancel')
+    button_1 = types.KeyboardButton('Yesterday')
+    button_2 = types.KeyboardButton('Today')
+    button_3 = types.KeyboardButton('Tomorrow')
+    keyboard.add(button_1, button_2, button_3, cancel_button)
     return keyboard
